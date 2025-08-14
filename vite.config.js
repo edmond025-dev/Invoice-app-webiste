6cb35b002bc03401532eb5b475051682280e6cd7
orca <edoch06@gmail.com>
2025-08-14 19:23:56 +0300

    added the index html to public folder instead

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : 'https://edmond025-dev.github.io/Invoice-app-webiste/',
+  base : 'https://github.com/edmond025-dev/Invoice-app-webiste/',
 })
